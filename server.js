49b821067e4dd3525bce1085474e1936347b1690
Pavun786 <pulsarpavuns786@gmail.com>
2024-01-08 08:19:44 -0800

    welcom msg Add

--- server.js
+++ server.js
@@ -36,6 +36,10 @@ app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
 
+app.get("/",(req,res)=>{
+  res.send("Welcome to Social Media Application")
+})
+
 const server = app.listen(port, () => {
   console.log(`server running on PORT${port}`);
 });
